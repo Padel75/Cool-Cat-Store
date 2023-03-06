71516603a674895a04492d333a88ed8d984a7754
Sabrina Cazzolato <sabrina.cazzolato.1@ulaval.ca>
2023-03-06 14:19:34 -0500

    feat: add basic password hashing

--- api/routes/login.py
+++ api/routes/login.py
@@ -1,7 +1,6 @@
 from flask import request, session, jsonify
 from domain.services.user_service import UserService
 from api.exceptions.missingParameterException import MissingParameterException
-from api.exceptions.invalidParameterException import InvalidParameterException
 from . import login_bp
 
 @login_bp.route("/login", methods=['POST'])
@@ -13,10 +12,9 @@ def login():
     username = login_infos["username"]
     password = login_infos["password"]
 
-    if not __check_credentials(username, password):
-        raise InvalidParameterException("username ou password est invalide")
-
     user_sevice = UserService()
+    user_sevice.validate_user_password(username, password)
+
     user_id = user_sevice.get_user_id(username)
     session['logged_in'] = True
     session['id'] = user_id
@@ -25,12 +23,3 @@ def login():
         "user_id": user_id
     }
     return jsonify(response), 200
-
-def __check_credentials(username, password):
-    user_sevice = UserService()
-    validPassword = user_sevice.get_user_password(username)
-    if validPassword is None:
-        return False
-    return validPassword == password #À sécuriser
-
-
