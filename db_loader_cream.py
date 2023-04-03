b6e0dadb53f566d788d81c568682586d44170a1c
Pascal <pascal.de-le-rue.1@ulaval.ca>
2023-04-03 17:04:21 -0400

    creation csv products

--- db_loader_cream.py
+++ db_loader_cream.py
@@ -1,36 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
+import csv
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 is_on_page = True
 no_page = 1
 
+
+names_list = []
+categories_list = []
+size_list = []
+prices_list = []
 while is_on_page:
     try:
         driver.get(f"https://www.saq.com/fr/produits?p={no_page}")
 
         names = driver.find_elements(By.CLASS_NAME, "product-item-link")
-        names_list = []
         for p in range(len(names)):
             names_list.append(names[p].text)
 
         descriptions = driver.find_elements(
             By.CLASS_NAME, "product-item-identity-format"
         )
-        categories_list = []
-        size_list = []
         for p in range(len(descriptions)):
             text = descriptions[p].text.split(" | ")
             categories_list.append(text[0].strip())
             size_list.append(text[1].strip())
 
         prices = driver.find_elements(By.CLASS_NAME, "price")
-        prices_list = []
         for p in range(len(prices)):
             prices_list.append(prices[p].text)
 
@@ -40,3 +40,11 @@ while is_on_page:
             is_on_page = False
     except:
         is_on_page = False
+
+with open("products.csv", "w") as f:
+    # create the csv writer
+    writer = csv.writer(f)
+
+    for i in range(len(names_list)):
+        row = [names_list[i], categories_list[i], size_list[i], prices_list[i]]
+        writer.writerow(row)
