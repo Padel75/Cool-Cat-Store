fee3038ce047c046dbf50ef601f3e841ca1e41fe
Sabrina Cazzolato <sabrina.cazzolato.1@ulaval.ca>
2023-04-06 13:02:47 -0400

    remove useless log and import

--- ui/src/api/products.js
+++ ui/src/api/products.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import { router } from "@/router";
 
 export function getProducts() {
-  console.log("getProducts() called");
   const url = `${axios.defaults.baseURL}/products`;
   return axios.get(url).catch(error => {
     console.log(error);
