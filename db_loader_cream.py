cd53496696c8a50269f6f9e48627e947896a033b
Pascal <pascal.de-le-rue.1@ulaval.ca>
2023-04-07 17:44:43 -0400

    ajout data customers

--- db_loader_cream.py
+++ db_loader_cream.py
@@ -2,49 +2,58 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import Chrome, ChromeOptions
 import csv
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-is_on_page = True
-no_page = 1
+def scrap_products_infos():
+    """Scrap products infos from SAQ website and save them in a csv file"""
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    names_list = []
+    categories_list = []
+    size_list = []
+    prices_list = []
+    image_list = []
+    for no_page in range(10):
+        try:
+            driver.get(f"https://www.saq.com/fr/produits?p={no_page}")
+
+            names_list = [name.text for name in driver.find_elements(By.CLASS_NAME, "product-item-link")]
+
+            description_list = [desc.text.split(" | ") for desc in
+                                driver.find_elements(By.CLASS_NAME, "product-item-identity-format")]
+
+            size_list = [desc[1].strip() for desc in description_list]
+
+            image_list = [image.get_attribute("src") for image in
+                          driver.find_elements(By.CLASS_NAME, "product-image-photo")]
+
+            prices_list = [price.text.replace('"', "") for price in driver.find_elements(By.CLASS_NAME, "price")]
+
+            categories_list = [desc[0].strip() for desc in description_list]
 
 
-names_list = []
-categories_list = []
-size_list = []
-prices_list = []
-while is_on_page:
-    try:
-        driver.get(f"https://www.saq.com/fr/produits?p={no_page}")
 
-        names = driver.find_elements(By.CLASS_NAME, "product-item-link")
-        for p in range(len(names)):
-            names_list.append(names[p].text)
+        except:
+            print(f"Page {no_page} not found")
 
-        descriptions = driver.find_elements(
-            By.CLASS_NAME, "product-item-identity-format"
-        )
-        for p in range(len(descriptions)):
-            text = descriptions[p].text.split(" | ")
-            categories_list.append(text[0].strip())
-            size_list.append(text[1].strip())
+    with open("products.csv", "w") as f:
+        writer = csv.writer(f)
 
-        prices = driver.find_elements(By.CLASS_NAME, "price")
-        for p in range(len(prices)):
-            price = prices[p].text.replace('"', "")
-            prices_list.append(price)
+        for no_page in range(len(names_list)):
+            row = [names_list[no_page], size_list[no_page], image_list[no_page], prices_list[no_page], categories_list[no_page]]
+            writer.writerow(row)
 
-        if no_page < 10:
-            no_page += 1
-        else:
-            is_on_page = False
-    except:
-        is_on_page = False
+    # push here in DB
 
-with open("products.csv", "w") as f:
-    writer = csv.writer(f)
+def scrap_customers_infos():
+    # Data generated with https://generatedata.com/generator, saved as csv file customers.csv
+    usernames_list = []
+    passwords_list = []
+    first_names_list = []
+    last_names_list = []
+    addresses_list = []
+    phone_numbers_list = []
+    emails_list = []
 
-    for i in range(len(names_list)):
-        row = [names_list[i], categories_list[i], size_list[i], prices_list[i]]
-        writer.writerow(row)
+    # push here in DB
