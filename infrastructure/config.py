debae297e5b004309847a087dd7d78817bc39161
Sabrina Cazzolato <sabrina.cazzolato.1@ulaval.ca>
2023-04-11 01:14:08 -0400

    fix: config

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -2,7 +2,7 @@
 class Config:
     MYSQL_HOST: str = 'localhost'
     MYSQL_USER: str = 'root'
-    MYSQL_PASSWORD: str = 'Willy006'
+    MYSQL_PASSWORD: str = 'MDP'
     MYSQL_DB: str = 'GLO2005_TP'
     MYSQL_PORT: int = 1433
     DATABASE_COMMANDS_FILE: str = 'infrastructure/commandes.sql'
