a1bec953e6bfa3461ed2ccde7002c302bc8eb071
Pascal <pascal.de-le-rue.1@ulaval.ca>
2023-04-15 14:39:47 -0400

    fix port

--- infrastructure/config.py
+++ infrastructure/config.py
@@ -2,7 +2,7 @@
 class Config:
     MYSQL_HOST: str = 'localhost'
     MYSQL_USER: str = 'root'
-    MYSQL_PASSWORD: str = '$98ba985'
+    MYSQL_PASSWORD: str = 'LOL'
     MYSQL_DB: str = 'GLO2005_TP'
     MYSQL_PORT: int = 1433
     DATABASE_COMMANDS_FILE: str = 'infrastructure/commandes.sql'
