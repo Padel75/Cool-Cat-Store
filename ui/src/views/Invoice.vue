748eedb33d561529ab96becd9d4c87f481e60886
NicManzaUL <nicholas.manzano.1@ulaval.ca>
2023-04-17 13:46:41 -0400

    only write total cost at bottom of invoice entry

--- ui/src/views/Invoice.vue
+++ ui/src/views/Invoice.vue
@@ -79,9 +79,13 @@ export default {
       table +=    `+${'-'.repeat(c_id.length)}+${'-'.repeat(c_date.length)}+${'-'.repeat(c_name.length)}+${'-'.repeat(c_price.length)}+${'-'.repeat(c_qty.length)}+${'-'.repeat(c_tcost.length)}+\n`;
 
       for (const invoice of invoices.invoices) {
-        for (const product of invoice.products) {
-          table += `| ${invoice.id.toString().padEnd(lstr_id)} | ${invoice.date.padEnd(lstr_date)} | ${product.name.padEnd(lstr_name)} | ${product.price.toFixed(2).padEnd(lstr_price)} | ${product.quantity.toString().padEnd(lstr_qty)} | ${invoice.total_cost.toFixed(2).padEnd(lstr_tcost)} |\n`;
-        }
+        for (const [index, product] of invoice.products.entries()) {
+          let totalCost='';
+            if(index == invoice.products.length-1){
+              totalCost = invoice.total_cost.toFixed(2)
+            }
+            table += `| ${invoice.id.toString().padEnd(lstr_id)} | ${invoice.date.padEnd(lstr_date)} | ${product.name.padEnd(lstr_name)} | ${product.price.toFixed(2).padEnd(lstr_price)} | ${product.quantity.toString().padEnd(lstr_qty)} | ${totalCost.padEnd(lstr_tcost)} |\n`;
+          }
         table +=`+${'-'.repeat(c_id.length)}+${'-'.repeat(c_date.length)}+${'-'.repeat(c_name.length)}+${'-'.repeat(c_price.length)}+${'-'.repeat(c_qty.length)}+${'-'.repeat(c_tcost.length)}+\n`;
       }
 
